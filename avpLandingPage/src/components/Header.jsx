3b86d4baea297a92f543b99ebfe1ab69357b313a
Dheeraj Shrivastav <dheerajshrivastav08@gmail.com>
2024-01-17 23:39:33 +0530

    Update Header component with new navigation links and styles

--- avpLandingPage/src/components/Header.jsx
+++ avpLandingPage/src/components/Header.jsx
@@ -5,38 +5,43 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <header style={{ backgroundColor: 'transparent' }}>
-            <nav className="bg-gray-800 flex flex-wrap items-center justify-between px-4 py-2">
-                <div className="flex items-center">
-                    <img src={avp_logo} alt="AVP Logo" className="h-8 w-30 mr-2" />
-                    <span className="text-white font-bold">AVP Group</span>
-                </div>
-                <input type="checkbox" id="menu-toggle" className="hidden" />
-                <label htmlFor="menu-toggle" className="text-white cursor-pointer">
-                    <svg className="h-6 w-6" viewBox="0 0 24 24">
-                        <path
-                            fill="currentColor"
-                            d="M4 6h16M4 12h16M4 18h16"
-                        />
-                    </svg>
-                </label>
-                <ul className="flex flex-col sm:flex-row sm:items-center sm:space-x-4 sm:space-y-0 sm:ml-auto">
-                    <li className="text-white">
-                        <Link to="/about">About</Link>
-                    </li>
-                    <li className="text-white">
-                        <Link to="/contact">Contact Us</Link>
-                    </li>
-                    <li className="text-white">
-                        <Link to="/services">Services</Link>
-                    </li>
-                    <li className="text-white">
-                        <Link to="/work">Our Work</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+      <header
+        className="fixed top-0 w-full z-50"
+        style={{ backgroundColor: 'transparent' }}
+      >
+        <nav className="bg-gray-800 flex flex-wrap items-center justify-between px-4 py-2">
+          <div className="flex items-center">
+            <img src={avp_logo} alt="AVP Logo" className="h-8 w-30 mr-2" />
+            <span className="text-white font-bold">AVP Group</span>
+          </div>
+          <input type="checkbox" id="menu-toggle" className="hidden" />
+          <label htmlFor="menu-toggle" className="text-white cursor-pointer">
+            <svg className="h-6 w-6" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </label>
+          <ul className=" flex flex-col sm:flex-row sm:items-center sm:space-x-4 sm:space-y-0 sm:ml-auto">
+            <li className="text-white">
+              <a href="/">Home</a>
+            </li>
+            <li className="text-white">
+              <a href="#service">Services</a>
+            </li>
+
+            <li className="text-white">
+              <a href="#about">About</a>
+            </li>
+            <li className="text-white">
+              <a href="#contact">Contact Us</a>
+            </li>
+
+            <li className="text-white">
+              <Link to="/work">Our Work</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    )
 };
 
 export default Header;
