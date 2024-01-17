c909bd26638907b85b6697e97280a1d53232ea33
Pratik Khaire <pratikkhaire2004.com>
2024-01-17 22:00:29 +0530

    footer color and changes done with contect flex

--- avpLandingPage/src/components/Footer.jsx
+++ avpLandingPage/src/components/Footer.jsx
@@ -5,12 +5,12 @@ import XIcon from '@mui/icons-material/X';
 import InstagramIcon from '@mui/icons-material/Instagram';
 const Footer = () => {
     return (
-      <footer className="bg-gray-800 text-white">
-        <div className="container mx-auto py-8">
+       <footer style={{ backgroundColor: '#7a797a' }} className="bg-gray-800 text-white">
+        <div className="container h-800">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                <div className='pb-28'>
+                <div className='pb-28 pl-20'  >
                         <div className="flex flex-col justify-between h-full">
-                          <div className="flex items-center">
+                          <div className="flex items-center pt-60">
                             <img
                               src={avp_logo}
                               alt="AVP Logo"
@@ -19,19 +19,21 @@ const Footer = () => {
                             <span className="text-white font-bold">AVP Group</span>
                           </div>
                         </div>
-                  <h3 className="text-lg font-bold">Contact Us</h3>
+                       
+                  <h3 className="text-lg font-bold ">Contact Us</h3>
                   <p>123 Construction Street</p>
                   <p>City, State, ZIP</p>
                   <p>Phone: 123-456-7890</p>
                   <p>Email: info@constructionwebsite.com</p>
+                  
                 </div>
-            <div>
+            <div className='pt-80'>
               <h3 className="text-lg font-bold">Follow Us</h3>
               <ul className="flex space-x-4">
                 <li>
                   <a href="#" className="text-white-500 hover:text-blue-700">
                     <FacebookIcon/>
-                    
+                  
                   </a>
                 </li>
                 <li>
@@ -47,7 +49,7 @@ const Footer = () => {
                 </li>
               </ul>
             </div>
-                <div className='pt-32'>
+                <div className='pt-32 pl-12 '>
                   <h3 className="text-lg font-bold">Services Offered</h3>
                   <ul className="list-disc list-inside">
                     <li>Construction Consultant</li>
