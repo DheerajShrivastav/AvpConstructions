a588f5f5663a945c088f18b5066b357ccb5d28c5
Dheeraj Shrivastav <dheerajshrivastav08@gmail.com>
2024-01-19 13:09:56 +0530

    Update grid layout and text alignment in OurServices component

--- avpLandingPage/src/components/Ourservices.jsx
+++ avpLandingPage/src/components/Ourservices.jsx
@@ -5,7 +5,7 @@ import Real from '../assets/Real.png';
 const OurServices = () => {
   return (
     <div>
-      <div id="service" className="grid grid-cols-4 gap-4">
+      <div id="service" className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
         {/* Service 1 */}
         <div>
           <img src={Real} alt="Construction Consultant" className="w-full" />
@@ -47,22 +47,22 @@ const OurServices = () => {
           <p className="text-center">Land Survey</p>
         </div>
       </div>
-      <div id="about">
-        <h3 className="text-2xl font-bold mb-4 p-0 0 15px">Why us?</h3>
-        <p className="text-gray-700 pb-15px">
+      <div id="about" className="text-center">
+        <h3 className="text-2xl font-bold mb-4">Why us?</h3>
+        <p className="text-gray-700 pb-4">
           ACS Construction Group was formed over 20 years ago to break boundaries and build excellence. We do this through being a people-first business.
         </p>
-        <p className="text-gray-700 pb-15px">
+        <p className="text-gray-700 pb-4">
           The majority of our team are employed directly by us and we always recruit and guide our employees based on our four core values: be innovative, be experts, be driven and be committed.
         </p>
-        <p className="text-gray-700 pb-15px">
+        <p className="text-gray-700 pb-4">
           Our values guide us all by continuously challenging our approach and pushing the boundaries to offer innovative, cutting-edge solutions.
         </p>
-        <p className="text-gray-700 pb-15">
+        <p className="text-gray-700 pb-4">
           We take pride in being highly knowledgeable experts in our field. Always committed to excellence, we act with drive and integrity to exceed our clients' expectations.
         </p>
       </div>
-      <div>
+      <div className="text-center">
         <Link to="/more-information">
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             More
